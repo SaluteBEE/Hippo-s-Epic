cec34e3d16258c9126c0756d85c1495ec4e2e865
Labrador52 <xuzhuo202004@outlook.com>
2026-02-09 12:49:42 +0800

    chore(ui): mark MainMenuPanel as deprecated
    Add [Obsolete] attribute to MainMenuPanel to indicate it has been
    superseded by the new MVVM system. Developers are now directed to use
    the MVVM architecture instead.

--- Assets/Scripts/UI/MainMenuPanel.cs
+++ Assets/Scripts/UI/MainMenuPanel.cs
@@ -1,5 +1,7 @@
+using System;
 using UnityEngine;
 
+[Obsolete("This class is deprecated. Please use the new MVVM class instead.")]
 public class MainMenuPanel : UIWindow
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
