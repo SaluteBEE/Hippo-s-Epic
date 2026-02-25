36e9309956a10b8b7bbf3ba4bfc1e79e8cd9fba6
Labrador52 <xuzhuo202004@outlook.com>
2026-02-25 20:40:47 +0800

    feat(ParallaxLayer): 添加 ParallaxLayer 类以实现基于 Z 轴的视差效果

---
+++ Assets/Scripts/Core/Level2D/Map/ParallaxLayer.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace Core.Level2D.Maps
+{
+    /// <summary>
+    /// 基于自身 Transform Z 轴 作为系数，在 Camera Moved 回调中乘算
+    /// </summary>
+    public class ParallaxLayer
+    {
+        private readonly Transform transform;
+        private readonly float scaleFactor;
+
+        public ParallaxLayer(Transform transform)
+        {
+            this.transform = transform;
+            scaleFactor = transform.position.z;
+        }
+
+        public void OnCameraMoved(Vector2 cameraOffset)
+        {
+            Vector2 layerOffset = cameraOffset * scaleFactor;
+            transform.position = (Vector3)layerOffset + new Vector3(0, 0, scaleFactor);
+        }
+    }
+}
