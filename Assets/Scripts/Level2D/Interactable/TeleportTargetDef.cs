5aac6b64de10501a8104e29e43fb3b721d2205f8
面包与自由 <166792776+BreadAndFreedom@users.noreply.github.com>
2026-05-31 18:13:45 +0800

    场景提交

--- Assets/Scripts/Level2D/Interactable/TeleportTargetDef.cs
+++ Assets/Scripts/Level2D/Interactable/TeleportTargetDef.cs
@@ -1,6 +1,6 @@
 ﻿// ============================================================
 // 自动生成文件 - 勿手动修改
-// 生成时间: 2026-05-24 23:45:38
+// 生成时间: 2026-05-31 18:09:48
 // 通过 Tools/Map/收集所有场景的传送目标 重新生成
 // ============================================================
 
@@ -23,12 +23,31 @@ public static class TeleportTargetDef
     {
         new SceneEntry
         {
+            sceneName = "Scene_Outer_City",
+            mapNames = new string[] { "Outer_City_Ground" },
+            interactables = System.Array.Empty<InteractableEntry>()
+        },
+        new SceneEntry
+        {
+            sceneName = "Scene_Punk_City",
+            mapNames = new string[] { "Punk_City_Ground" },
+            interactables = System.Array.Empty<InteractableEntry>()
+        },
+        new SceneEntry
+        {
+            sceneName = "Scene_Sewer",
+            mapNames = new string[] { "Sewer_Surface" },
+            interactables = System.Array.Empty<InteractableEntry>()
+        },
+        new SceneEntry
+        {
             sceneName = "Scene_Staff_Lounge",
             mapNames = new string[] { "Staff Lounge", "Underground Boxing Gym" },
             interactables = new InteractableEntry[]
             {
                 new InteractableEntry { entityId = "Main_Cup", mapName = "Staff Lounge" },
                 new InteractableEntry { entityId = "trans_Teleporter_To_Gym", mapName = "Staff Lounge" },
+                new InteractableEntry { entityId = "Main_Challenger 2", mapName = "Underground Boxing Gym" },
                 new InteractableEntry { entityId = "NPC_NPC_Coach", mapName = "Underground Boxing Gym" },
                 new InteractableEntry { entityId = "trans_Transporter", mapName = "Underground Boxing Gym" }
             }
